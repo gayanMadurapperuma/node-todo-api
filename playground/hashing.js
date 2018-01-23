04167867dae26c1ff0bab350d1c54f6db2ed8dd2
gayan madurapperuma <madurapperumamags@gmail.com>
2018-01-23 23:04:40 +0530

    jwt security testing

---
+++ playground/hashing.js
@@ -0,0 +1,27 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+/*
+    JWT authenticatjon importance for identify data manupulation..
+    this is some of signature.
+    we use 2 function mainly JWT authentication
+    jwt.sign(),
+    jwt.verify(),
+*/
+
+// var message = 'this is new one for th';
+// var hash = SHA256(message).toString();
+
+// console.log(hash);
+
+var data = {
+    id : 12,
+    name : 'gayan madurapperuma',
+    age : '25'
+}
+
+var hs = jwt.sign(data, 'sinxcosx=tanx');
+console.log(hs);
+
+var _decode = jwt.verify(hs,'sinxcosx=tanx');
+console.log(_decode);
