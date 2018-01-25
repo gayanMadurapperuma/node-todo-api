b726e1ba4772080961d4b1cb3a3673879e8901a4
gayan madurapperuma <madurapperumamags@gmail.com>
2018-01-26 01:50:17 +0530

    bcryptjs test

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,18 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = 'abc123@#';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password,salt,(err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashingPW = '$2a$10$83ghlaqiqoZHJcS9oDYG3Oc5J93e7J5hJCrKqzV0Uaczs4SgEHwte';
+bcrypt.compare(password, hashingPW, (err,res) => {
+    console.log(res);
+});
 
 /*
     JWT authenticatjon importance for identify data manupulation..
@@ -14,25 +27,25 @@ const jwt = require('jsonwebtoken');
 
 // console.log(hash);
 
-var data = {
-    id : 12,
-    name : 'gayan madurapperuma',
-    age : '25'
-}
+// var data = {
+//     id : 12,
+//     name : 'gayan madurapperuma',
+//     age : '25'
+// }
 
-var access = 'auth';
-var id = 232348028304;
+// var access = 'auth';
+// var id = 232348028304;
 
-var test = {
-    _id : id.toString(16),
-    id,
-    access
-}
+// var test = {
+//     _id : id.toString(16),
+//     id,
+//     access
+// }
 
-return console.log(test);
+// return console.log(test);
 
-var hs = jwt.sign(data, 'sinxcosx=tanx');
-console.log(hs);
+// var hs = jwt.sign(data, 'sinxcosx=tanx');
+// console.log(hs);
 
-var _decode = jwt.verify(hs,'sinxcosx=tanx');
-console.log(_decode);
+// var _decode = jwt.verify(hs,'sinxcosx=tanx');
+// console.log(_decode);
